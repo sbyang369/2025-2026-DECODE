4cd2674136533843d71713cf70ca66e0c44ed6f5
sbyang369 <139173281+sbyang369@users.noreply.github.com>
2026-01-17 08:08:39 -0500

    Update teleop.java
    meet 4

--- teleop.java
+++ teleop.java
@@ -16,51 +16,58 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class DecodeMain extends LinearOpMode {
     private Blinker control_Hub;
   
+    
     private DcMotor bottomL = null;
     private DcMotor bottomR = null;
     private DcMotor topL = null;
     private DcMotor topR = null;
-    private DcMotor intake = null; 
+    
+    
+    private DcMotor intake = null;
+    private DcMotor leftOutake = null; 
+    private DcMotor rightOutake = null; 
     
     private CRServo wheel = null;
+    private Servo wrist = null;
     
     public ElapsedTime runtime = new ElapsedTime();
     public boolean buttonWasPressed = false;
     public double reverseStartTime = 0.0;
     public final double REVERSE_DURATION = 0.5;
     
-    private Servo wrist = null;
-    private Servo wheel = null;
-    
-    private str blinker = "orange"
     @Override
     public void runOpMode() {
         // Initialize hardware
-        control_hub = hardwareMap.get(Blinker.class, "Control Hub");
-        
         topL = hardwareMap.get(DcMotor.class, "topL");
         topR = hardwareMap.get(DcMotor.class, "topR");
         bottomL = hardwareMap.get(DcMotor.class, "bottomL");
         bottomR = hardwareMap.get(DcMotor.class, "bottomR");
         
         intake = hardwareMap.get(DcMotor.class, "intake");
+        leftOutake = hardwareMap.get(DcMotor.class, "leftOutake");
+        rightOutake = hardwareMap.get(DcMotor.class, "rightOutake");
+        
         wrist = hardwareMap.get(Servo.class, "wrist");
-        wheel = hardwareMap.get(Servo.class, "wheel");
+        wheel = hardwareMap.get(CRServo.class, "wheel");
 
-        // Reverse motor direction
-        bottomL.setDirection(DcMotor.Direction.REVERSE);
-        topL.setDirection(DcMotor.Direction.REVERSE);
+        
+        topL.setDirection(DcMotor.Direction.FORWARD);
+        topR.setDirection(DcMotor.Direction.REVERSE);
+        bottomL.setDirection(DcMotor.Direction.FORWARD);
         telemetry.addData("Status", "Initialized");
+        
+        
         telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
-            // Normal movement and other functions
+            
             double y = gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x * -1.1; // Adjust for strafing
-            double rx = gamepad1.right_stick_x;
-            double maxPower = 0.5;
+            double x = -gamepad1.left_stick_x * 1.1; // Adjust for strafing
+            double rx = -gamepad1.right_stick_x;
+            
+            double maxPower = 0.7;
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator * maxPower;
             double backLeftPower = (y - x + rx) / denominator * maxPower;
@@ -72,15 +79,38 @@ public class DecodeMain extends LinearOpMode {
             topR.setPower(frontRightPower);
             bottomR.setPower(backRightPower); 
             
+            
             // Intake 
-            if (gamepad2.y) {
+            if (gamepad2.a) {
+                intake.setPower(1);
+            } else if (gamepad2.dpad_down) {
                 intake.setPower(-1);
             } else {
                 intake.setPower(0);
             }
             
-            //Wheel
-            // manual changing
+            
+            // Outtake 
+            if (gamepad2.dpad_up) {
+                rightOutake.setPower(-0.8); //change power
+                leftOutake.setPower(0.8);
+                buttonWasPressed = true;
+            } else if (buttonWasPressed) {
+               if (runtime.seconds() < reverseStartTime + REVERSE_DURATION) {
+                    rightOutake.setPower(0.1);
+                    leftOutake.setPower(-0.1);
+                } else {
+                    rightOutake.setPower(0);
+                    leftOutake.setPower(0);
+                    buttonWasPressed = false;
+                }
+            } else {
+                rightOutake.setPower(0);
+                leftOutake.setPower(0);
+                reverseStartTime = runtime.seconds();
+            }
+            
+            // wheel manual changing
             if (gamepad2.dpad_left) {
                 wheel.setPower(-.2);
             } else if (gamepad2.dpad_right) {
@@ -92,84 +122,21 @@ public class DecodeMain extends LinearOpMode {
             //timed-based changing
             if (gamepad2.x) {
                 wheel.setPower(0.2);
-                sleep(550); // CHANGE THIS FOR 60 DEGREES
+                sleep(1125); // CHANGE THIS FOR 60 DEGREES. works as of 1/16/26
                 wheel.setPower(0);
             } else if (gamepad2.b) {
                 wheel.setPower(-0.2);
-                sleep(550);
+                sleep(1125);
+                wheel.setPower(0);
+            } else {
                 wheel.setPower(0);
             }
-
-            boolean status = false;
-            // Outtake 
-            if (gamepad2.dpad_up) {
-                rightOutake.setPower(0.6); //change power
-                leftOutake.setPower(-0.6);
-                buttonWasPressed = true;
-            } else if (buttonWasPressed) {
-                if (runtime.seconds() < reverseStartTime + REVERSE_DURATION) {
-                    rightOutake.setPower(-0.1);
-                    leftOutake.setPower(0.1);
-                } else {
-                    rightOutake.setPower(0);
-                    leftOutake.setPower(0);
-                    buttonWasPressed = false;
-                }
-            } else {
-                rightOutake.setPower(0);
-                leftOutake.setPower(0);
-                reverseStartTime = runtime.seconds();
-            }
-
-            // Wrist
-            if (gamepad2.x) {
-                telemetry.addData("Position", wrist.getPosition());
-                wrist.setPosition(0);
-            } else {
-                telemetry.addData("Position", wrist.getPosition());
-                wrist.setPosition(0.8);
-            }
             
-            // Wheel
-            if (gamepad2.dpad_left) {
-                wheel.setPosition(0);
-            } else if (gamepad2.dpad_right) {
-                wheel.setPosition(1);
-            }
-
-            // wyatt modified Outtake 
-            if (gamepad2.dpad_up) {
-                if (blinker == "orange") {
-                    rightOutake.setPower(0.99);
-                    leftOutake.setPower(-0.99);
-                } else if (blinker == "red") {
-                    rightOutake.setPower(0.99);
-                    leftOutake.setPower(-0.99);
-                } else {
-                    rightOutake.setPower(0.99);
-                    leftOutake.setPower(-0.99);
+            // Wrist
+            if (gamepad2.y) {
+                wrist.setPosition(1); //need to ADJUST based on ideal wrist launch position
             } else {
-                rightOutake.setPower(0);
-                leftOutake.setPower(0);
-            }
-
-            if (blinker == "orange") {
-                control_hub.setConstant(Color.ORANGE);
-            } else if (blinker == "red") {
-                control_hub.setConstant(Color.RED);
-            } else {
-                control_hub.setConstant(Color.GREEN);
-            }
-                
-            //crazy button
-            if (gamepad1.a) {
-                if (blinker == "orange") {
-                    str blinker = "red";
-                } else if (blinker == "red) {
-                    str blinker = "green";
-                } else {
-                    str blinker = "orange";
-                }
+                wrist.setPosition(0); //may need to ADJUST
             }
             
             telemetry.update();
