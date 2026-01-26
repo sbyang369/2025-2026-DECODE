1c04ab3ee4e2d5eff8282ed0d2dd1b0a2ef67d01
sbyang369 <139173281+sbyang369@users.noreply.github.com>
2026-01-26 11:58:30 -0500

    Update teleop.java

--- teleop.java
+++ teleop.java
@@ -37,84 +37,66 @@ public class DecodeMain extends LinearOpMode {
     
     @Override
     public void runOpMode() {
-        // Initialize hardware
         topL = hardwareMap.get(DcMotor.class, "topL");
         topR = hardwareMap.get(DcMotor.class, "topR");
         bottomL = hardwareMap.get(DcMotor.class, "bottomL");
         bottomR = hardwareMap.get(DcMotor.class, "bottomR");
-        
         intake = hardwareMap.get(DcMotor.class, "intake");
         leftOutake = hardwareMap.get(DcMotor.class, "leftOutake");
         rightOutake = hardwareMap.get(DcMotor.class, "rightOutake");
-        
         wrist = hardwareMap.get(Servo.class, "wrist");
         wheel = hardwareMap.get(CRServo.class, "wheel");
 
-        
         topL.setDirection(DcMotor.Direction.FORWARD);
         topR.setDirection(DcMotor.Direction.REVERSE);
         bottomL.setDirection(DcMotor.Direction.FORWARD);
         telemetry.addData("Status", "Initialized");
         
-        
         telemetry.update();
-
         waitForStart();
 
         while (opModeIsActive()) {
-            
+            //drivetrain
             double y = gamepad1.left_stick_y;
-            double x = -gamepad1.left_stick_x * 1.1; // Adjust for strafing
+            double x = -gamepad1.left_stick_x * 1.1;
             double rx = -gamepad1.right_stick_x;
-            
             double maxPower = 0.7;
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator * maxPower;
             double backLeftPower = (y - x + rx) / denominator * maxPower;
             double frontRightPower = (y - x - rx) / denominator * maxPower;
             double backRightPower = (y + x - rx) / denominator * maxPower;
-
             topL.setPower(frontLeftPower);
             bottomL.setPower(backLeftPower);
             topR.setPower(frontRightPower);
             bottomR.setPower(backRightPower); 
             
-            
             // Intake 
             if (gamepad2.a) {
-                intake.setPower(1);
-            } else if (gamepad2.dpad_down) {
                 intake.setPower(-1);
+            } else if (gamepad2.dpad_down) {
+                intake.setPower(1);
             } else {
                 intake.setPower(0);
             }
             
-            
             // Outtake 
-            if (gamepad2.dpad_up) {
-                rightOutake.setPower(-0.8); //change power
-                leftOutake.setPower(0.8);
-                buttonWasPressed = true;
-            } else if (buttonWasPressed) {
-               if (runtime.seconds() < reverseStartTime + REVERSE_DURATION) {
-                    rightOutake.setPower(0.1);
-                    leftOutake.setPower(-0.1);
-                } else {
-                    rightOutake.setPower(0);
-                    leftOutake.setPower(0);
-                    buttonWasPressed = false;
-                }
+            if (gamepad2.left_bumper) {
+                rightOutake.setPower(0.50);
+                leftOutake.setPower(-0.50);
+            } else if (gamepad2.left_trigger > 0.2) {
+                rightOutake.setPower(0.95);
+                leftOutake.setPower(-0.95);
             } else {
                 rightOutake.setPower(0);
                 leftOutake.setPower(0);
-                reverseStartTime = runtime.seconds();
             }
             
             // wheel manual changing
             if (gamepad2.dpad_left) {
-                wheel.setPower(-.2);
+                wheel.setPower(-.5);
             } else if (gamepad2.dpad_right) {
-                wheel.setPower(.2);
+                wheel.setPower(.5);
             } else {
                 wheel.setPower(0);
             }
@@ -122,7 +104,7 @@ public class DecodeMain extends LinearOpMode {
             //timed-based changing
             if (gamepad2.x) {
                 wheel.setPower(0.2);
-                sleep(1125); // CHANGE THIS FOR 60 DEGREES. works as of 1/16/26
+                sleep(1125); // CHANGE THIS FOR 120 DEGREES. works as of 1/16/26
                 wheel.setPower(0);
             } else if (gamepad2.b) {
                 wheel.setPower(-0.2);
@@ -134,9 +116,9 @@ public class DecodeMain extends LinearOpMode {
             
             // Wrist
             if (gamepad2.y) {
-                wrist.setPosition(1); //need to ADJUST based on ideal wrist launch position
+                wrist.setPosition(0.3); //need to ADJUST based on ideal wrist launch position
             } else {
-                wrist.setPosition(0); //may need to ADJUST
+                wrist.setPosition(0.6); //may need to ADJUST
             }
             
             telemetry.update();
